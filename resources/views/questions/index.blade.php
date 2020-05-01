66ee659a2ef18bbcfe802a69aa84b652380735d8
aymen <moniakamli95@gmail.com>
2020-05-01 16:08:32 +0200

    Display all questions

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">All Questions</div>
+
+                <div class="card-body">
+                    @foreach ($questions as $question)
+                        <div class="media">
+                            <div class="media-body">
+                            <h4 class="mt-0"> {{ $question->title}}</h4>
+                        {{ str_limit($question->body, 250) }}    
+                        </div>
+                        </div>
+                        <hr>
+                    @endforeach
+                        <div class="mx-auto">
+                            {{ $questions->links() }}
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
