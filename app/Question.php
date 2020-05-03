fe213beef939062c0a8b0a9ffa94df1310a84e10
aymen <moniakamli95@gmail.com>
2020-05-03 16:02:01 +0200

    Design answers table schema and rename answers column

--- app/Question.php
+++ app/Question.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     protected $fillable = ['title', 'body']; 
+
+
     public function user() {
         return $this->belongsTo(User::class);
     }
@@ -26,7 +28,7 @@ class Question extends Model
     }
 
     public function getStatusAttribute() {
-        if ($this->answers > 0 ) {
+        if ($this->answers_count > 0 ) {
             if ($this->best_answer_id) {
                 return "answered-accepted";
             }
@@ -38,5 +40,10 @@ class Question extends Model
 
     public function getBodyHtmlAttribute() {
         return \Parsedown::instance()->text($this->body);
+    }
+
+    public function answers() {
+        return $this->hasMany(Answer::class);
+
     }
 }
