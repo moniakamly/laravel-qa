7c80c4f349381f1c29aaad68c77e61bf9e220a79
aymen <moniakamli95@gmail.com>
2020-05-05 17:11:51 +0200

    Implement Update answer

--- resources/views/answers/_create.blade.php
+++ resources/views/answers/_create.blade.php
@@ -10,12 +10,12 @@
             <form action="{{ route('questions.answers.store', $question->id )}}" method="post">
                    @csrf
                     <div class="form-group">
-                    <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" name="body" rows="7"></textarea>
-                    @if ($errors->has('body')) 
-                        <div class="invalid-feedback">
-                        <strong>{{$errors->first('body') }}</strong>
-                        </div>
-                    @endif
+                        <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" name="body" rows="7"></textarea>
+                        @if ($errors->has('body')) 
+                            <div class="invalid-feedback">
+                            <strong>{{$errors->first('body') }}</strong>
+                            </div>
+                        @endif
                     </div>
                    <div class="form-group">
                        <button type="submit" class="btn btn-lg btn-outline-primary">Submit</button>
